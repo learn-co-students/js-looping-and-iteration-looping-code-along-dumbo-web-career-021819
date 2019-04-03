82202958a24f1d0a6ed3242c6cf5450ea45e4256
1-8192 <allegranzia@gmail.com>
2019-04-03 09:03:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function printBadges(empArray) {
+  for (let i=0; i < empArray.length; i++) {
+    console.log(`Welcome ${empArray[i]}! You are employee #${i + 1}.`)
+  }
+  return empArray;
+};
+
+function tailsNeverFails() {
+  let i=0;
+
+  while (Math.random() >= 0.5) {
+    i ++;
+  }
+  return `You got ${i} tails in a row!`;
+}
