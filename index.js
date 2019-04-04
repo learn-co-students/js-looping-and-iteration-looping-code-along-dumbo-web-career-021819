8aab107ea22ed003ad40a8867b130894da7a9bd3
campbelllssoup <matjocampbell@gmail.com>
2019-04-04 12:07:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const printBadges = ( (array) => {
+  for (i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`);
+  };
+  return array;
+});
+
+
+function tailsNeverFails() {
+  let random_num = 1.1;
+  let count = 0;
+  while (random_num > 0.4) {
+    random_num = Math.floor(Math.random() * 10) / 10;
+    count += 1;
+  };
+  return `You got ${count-1} tails in a row!`;
+};
