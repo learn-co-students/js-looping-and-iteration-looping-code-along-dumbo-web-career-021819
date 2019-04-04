108d1e69f9c9988c0657192bea6f409f1013b841
Jaela Wesley <github email address>
2019-04-04 19:50:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(names){
+    for(i = 0; i < names.length; i++){
+        console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
+    }
+    return names
+}
+
+
+
+function tailsNeverFails() {
+    coinFlip = Math.random() >= .5;
+
+    counter = 0
+    while (coinFlip){
+        counter++
+    }
+    return `You got ${counter} tails in a row`
+}
